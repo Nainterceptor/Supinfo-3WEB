22feb475896aaa239f1c46cc4457445e73f9c4f0
Gaël Demette <demette.gael@gmail.com>
2014-11-24 14:25:37 +0100

    Step 4 - Refactoring Socket.io

--- index.js
+++ index.js
@@ -1,73 +1,34 @@
-var http = require('http');
-var fs = require('fs');
-var WebSocketServer = require('websocket').server;
+var app = require('http').createServer(handler)
+    , io = require('socket.io').listen(app)
+    , fs = require('fs');
 
-var messages = [];
+app.listen(1337);
 
-var responses = [];
-
-var webServer = http.createServer(function (request, response) {
-
-    if (request.url == '/messages') {
-        response.end(JSON.stringify(messages));
-    }
-
-    else if (request.url == '/message') {
-        var message = "";
-
-        request.on('data', function (datas) {
-            message += datas;
-        });
-
-        request.on('end', function () {
-            try {
-                var newMessage = JSON.parse(message);
-                messages.push(newMessage.message);
-                response.end();
-
-                responses.forEach(function (aReponse) {
-                    aReponse.write('data: ' + newMessage.message + '\n\n');
-                });
-            }
-            catch (e) {
-                response.end();
-            }
-        });
-    }
-
-    else if (request.url == '/jquery-2.1.1.min.js') {
-        fs.readFile('jquery-2.1.1.min.js', function (err, data) {
-            response.end(data);
-        })
+function handler(req, res) {
+    if (req.url == '/jquery-2.1.1.min.js') {
+        fs.readFile(__dirname + '/jquery-2.1.1.min.js',
+            function (err, data) {
+                res.writeHead(200);
+                res.end(data);
+            });
     }
     else {
-        fs.readFile('index.html', function (err, data) {
-            response.end(data);
-        })
+        fs.readFile(__dirname + '/index.html',
+            function (err, data) {
+                res.writeHead(200);
+                res.end(data);
+            });
     }
+}
 
-});
-webServer.listen(1337);
-
-var wsServer = new WebSocketServer({
-    httpServer: webServer,
-    autoAcceptConnections: false
-});
-var connections = [];
-wsServer.on('request', function (req) {
-    var connection = req.accept('echo-protocol', req.origin);
-    console.log(new Date() + ' Peer ' + connection.remoteAddress + ' connected.');
-
-    connections.push(connection);
-    console.log(new Date() + ' ' + connections.length + ' peers connected.');
-
-    connection.on('message', function (message) {
-        var msg = message.utf8Data;
-        console.log(new Date() + ' Received message: ' + msg);
-        messages.push(msg);
-
-        connections.forEach(function (destination) {
-            destination.sendUTF(msg);
-        });
+io.sockets.on('connection', function (socket) {
+    socket.on('join', function (data) {
+        io.sockets.emit('join', data);
+    });
+    socket.on('message', function (data) {
+        io.sockets.emit('message', data);
+    });
+    socket.on('idle', function (data) {
+        io.sockets.emit('idle', data);
     });
 });
