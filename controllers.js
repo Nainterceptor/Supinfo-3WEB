47747325484471eb811b4c781b62d33cf844d1c4
Gaël Demette <demette.gael@gmail.com>
2016-04-07 11:58:27 +0200

    Step 2 : Search, sort, limit

--- controllers.js
+++ controllers.js
@@ -1,12 +1,16 @@
 "use strict";
 //controllers.js
 AngularApp.controller("ProductsCtrl", function($scope) {
+    $scope.order = '';
+
     $scope.products = [
         new Product()
     ];
+
     $scope.newProduct = function() {
         $scope.products.push(new Product())
     };
+
     $scope.removeProduct = function(product) {
         var index = $scope.products.indexOf(product);
         $scope.products.splice(index, 1);
